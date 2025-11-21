d8d0479d638ae5c1e61b4173334dc8ce53d4bdef
mateoalfaro <liquidsuper2@gmail.com>
2025-11-20 18:49:03 -0600

    Enable experimental Gnome features
    Adds gnome extensions and enables experimental Gnome features in dconf.

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -4,9 +4,27 @@
   # As of 25.11
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.appindicator
+  ];
 
   environment.gnome.excludePackages = with pkgs; [
     gnome-tour
     gnome-user-docs
   ];
+
+  # Experimental features
+  programs.dconf.profiles.user.databases = [
+      {
+        settings = {
+          "org/gnome/mutter" = {
+            experimental-features = [
+              "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
+              "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
+              "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
+            ];
+          };
+        };
+      }
+    ];
 }
